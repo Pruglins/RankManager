2f21c9b49838bb5c7e2c7e3b61b1c7e67438f86d
Program132 <clement.gauthier132@orange.fr>
2023-05-21 01:07:59 +0200

    Ajout et suppression d'une permission

---
+++ src/main/java/fr/program/cmds/AddPermission.java
@@ -0,0 +1,42 @@
+package fr.program.cmds;
+
+import fr.program.Main;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class AddPermission implements CommandExecutor {
+    private final Main plugin;
+
+    public AddPermission(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("addperm")) {
+            if (sender instanceof Player player) {
+                if (args.length >= 2) {
+                    String name = args[0];
+                    String permission = args[1];
+
+                    FileConfiguration config = plugin.getConfig();
+                    List<String> permissions = config.getStringList("ranks." + name + ".permissions");
+                    permissions.add(permission);
+                    config.set("ranks." + name + ".permissions", permissions);
+                    plugin.saveConfig();
+                    return true;
+                } else {
+                    player.sendMessage("[" + ChatColor.BLUE + "Rank Manager" + ChatColor.WHITE + "] "
+                            + "Veuillez préciser le nom du grade et la permission que vous voulez ajouter.");
+                }
+            }
+        }
+        return false;
+    }
+}
