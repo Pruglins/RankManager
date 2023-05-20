a937cbae5b453c967074aa47b5d99353632fc0a0
Program132 <clement.gauthier132@orange.fr>
2023-05-21 00:23:25 +0200

    Update CommandsManager.java

--- src/main/java/fr/program/CommandsManager.java
+++ src/main/java/fr/program/CommandsManager.java
@@ -1,13 +1,13 @@
 package fr.program;
 
-import fr.program.cmds.Create;
-import fr.program.cmds.List;
-import fr.program.cmds.SetPlayerRank;
+import fr.program.cmds.*;
 
 public class CommandsManager {
     public static void init(Main plugin) {
         plugin.getCommand("create").setExecutor(new Create(plugin));
         plugin.getCommand("list").setExecutor(new List(plugin));
         plugin.getCommand("spr").setExecutor(new SetPlayerRank(plugin));
+        plugin.getCommand("scr").setExecutor(new SetColorRank(plugin));
+        plugin.getCommand("snr").setExecutor(new SetNameRank(plugin));
     }
 }
