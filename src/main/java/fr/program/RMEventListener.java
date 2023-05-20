f8b5ec412468ead26f72ad8a2423a48734899e3e
Program132 <clement.gauthier132@orange.fr>
2023-05-21 00:32:54 +0200

    Update RMEventListener.java

--- src/main/java/fr/program/RMEventListener.java
+++ src/main/java/fr/program/RMEventListener.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class RMEventListener implements Listener {
     private final Main plugin;
@@ -24,5 +25,14 @@ public class RMEventListener implements Listener {
 
         event.setMessage("[" + rank_color + rank_name + ChatColor.WHITE + "] " + message);
         event.setCancelled(false);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        FileConfiguration config = plugin.getConfig();
+        String rank_name = config.getString("players." + player.getName() + ".rank");
+        String rank_color = config.getString("ranks." + rank_name + ".color");
+        event.setJoinMessage("[" + rank_color + rank_name + ChatColor.WHITE + "] " + ChatColor.BOLD + player.getName() + ChatColor.RESET + " a rejoint le serveur !");
     }
 }
