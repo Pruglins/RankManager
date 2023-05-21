82aeffeb3449d6833ce19e763ae8742f49e701e7
Program132 <clement.gauthier132@orange.fr>
2023-05-21 14:59:11 +0200

    Retire/Ajout les nouvelles permissions directement sur le joueur

--- src/main/java/fr/program/cmds/AddPermission.java
+++ src/main/java/fr/program/cmds/AddPermission.java
@@ -1,12 +1,15 @@
 package fr.program.cmds;
 
 import fr.program.Main;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionAttachment;
 
 import java.util.List;
 
@@ -17,6 +20,44 @@ public class AddPermission implements CommandExecutor {
         this.plugin = plugin;
     }
 
+    private boolean itExist(String r) {
+        FileConfiguration config = plugin.getConfig();
+        ConfigurationSection allRanksSection = config.getConfigurationSection("ranks");
+        if (allRanksSection != null) {
+            if (allRanksSection.contains(r)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void updatePerms(FileConfiguration config, String rank_name) {
+        ConfigurationSection playersSection = config.getConfigurationSection("players");
+        if (playersSection != null) {
+            for (String player : playersSection.getKeys(false)) {
+                ConfigurationSection player_sec = playersSection.getConfigurationSection(player);
+                if (player_sec.contains("rank") && player_sec.getString("rank") != null) {
+                    String rank = player_sec.getString("rank");
+                    if (rank.equals(rank_name)) {
+                        Player p = Bukkit.getPlayer(player_sec.getName());
+
+                        if (p != null) {
+                            List<String> rank_perms = config.getStringList("ranks." + rank + ".permissions");
+
+                            PermissionAttachment perm_attachment = p.addAttachment(plugin);
+
+                            for (String perm : rank_perms) {
+                                perm_attachment.setPermission(perm, true);
+                            }
+                        }
+
+                        System.out.println(player_sec.getName());
+                    }
+                }
+            }
+        }
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("addperm")) {
@@ -25,14 +66,21 @@ public class AddPermission implements CommandExecutor {
                     String name = args[0];
                     String permission = args[1];
 
-                    FileConfiguration config = plugin.getConfig();
-                    List<String> permissions = config.getStringList("ranks." + name + ".permissions");
-                    permissions.add(permission);
-                    config.set("ranks." + name + ".permissions", permissions);
-                    plugin.saveConfig();
-                    player.sendMessage("[" + ChatColor.BLUE + "Rank Manager" + ChatColor.WHITE + "] "
-                            + "La permission " + permission + " a été ajouté au rang " + config.getString("ranks." + name + ".color") + name + ChatColor.WHITE + ".");
-                    return true;
+                    if (itExist(name)) {
+                        FileConfiguration config = plugin.getConfig();
+                        List<String> permissions = config.getStringList("ranks." + name + ".permissions");
+                        permissions.add(permission);
+                        config.set("ranks." + name + ".permissions", permissions);
+                        plugin.saveConfig();
+                        updatePerms(config, name);
+                        player.sendMessage("[" + ChatColor.BLUE + "Rank Manager" + ChatColor.WHITE + "] "
+                                + "La permission " + permission + " a été ajouté au rang " + config.getString("ranks." + name + ".color") + name + ChatColor.WHITE + ".");
+                        return true;
+                    } else {
+                        player.sendMessage("[" + ChatColor.BLUE + "Rank Manager" + ChatColor.WHITE + "] "
+                                + "Le rang " + ChatColor.RED + name + ChatColor.WHITE + " n'existe pas.");
+                        return false;
+                    }
                 } else {
                     player.sendMessage("[" + ChatColor.BLUE + "Rank Manager" + ChatColor.WHITE + "] "
                             + "Veuillez préciser le nom du grade et la permission que vous voulez ajouter.");
